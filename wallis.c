2f22dd37a3197a53318ae7ae3737b590ceee8587
Cerinseb <84723277+Cerinseb@users.noreply.github.com>
2021-06-04 09:42:05 +0300

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -7,7 +7,7 @@ float wallis_pi(int);
 
 int main(void) {
   float pi;
-  for (int i=0; i<5; i++) {
+  for (int i=0; i<5; i++) {{
     pi = wallis_pi(i);
     if (!(fabs(pi - M_PI) > 0.15)) {
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
@@ -23,4 +23,14 @@ int main(void) {
     }
   }
 }
+float wallis_pi(int n)
+{
+  float product;
+  product =2.0;
+  int i,j ;
+  for (i=1;i<n+1;i++)
+  product=(product*4*i*i)/(4*i*i-1);
+}return product;
+}
+  
 

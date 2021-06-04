7ad6bcde43bf549cf6e5115b88a59d31f8c0c2cc
Cerinseb <84723277+Cerinseb@users.noreply.github.com>
2021-06-04 10:16:47 +0300

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -25,13 +25,14 @@ int main(void) {
 }
 float wallis_pi(int n)
 {
-  float product;
-  product =2.0;
-  int i,j ;
-  for (i=1;i<n+1;i++)
+ float product;
+ product=2.0;
+ int i,j;
+ for(i=1;i<n+1;i++)
+ {
   product=(product*4*i*i)/(4*i*i-1);
-}
-return product;
+ }
+ return product;
 }
   
 
